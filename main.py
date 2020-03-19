01031a63f368b7f5cdbaea5995875be16b734ddf
Evan Nemetz <37675159+enemetz@users.noreply.github.com>
2020-03-19 13:23:21 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def singleMap(string1, string2):
+    dict = {}
+
+    # Only mappable if string1 is shorter than string2
+    if len(string1) > len(string2):
+        return False
+
+    # iterate through string1, place character in dict. If in dictionary, return false
+    for i in range(len(string1)):
+        if string1[i] not in dict:
+            dict[string1[i]] = string2[i]
+        else:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    str1 = sys.argv[1]
+    str2 = sys.argv[2]
+    print(singleMap(str1, str2))
